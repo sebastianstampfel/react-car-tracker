51980069f72479b02fd8730a931d2e4658825fc8
Sebastian Stampfel <sebastian.stampfel@gmail.com>
2021-04-20 15:17:43 +0200

    Adds feature to update category names

--- src/features/categories/CategoryList.js
+++ src/features/categories/CategoryList.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import { ListGroup, Col, Row } from 'react-bootstrap'
+import { ListGroup, Col, Row, Form } from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
-import { FaTimes } from 'react-icons/fa'
-import { categoriesRemoved } from './categoriesSlice'
+import { FaTimes, FaPencilAlt, FaSave } from 'react-icons/fa'
+import { categoriesRemoved, categoriesUpdated } from './categoriesSlice'
 import { clearCategory } from '../cars/carsSlice'
 
 
@@ -10,6 +10,13 @@ import { clearCategory } from '../cars/carsSlice'
 const CategoryList = () => {
     const [showDeleteAlert, setShowDeleteAlert] = useState(false);
     const toggleShowDeleteAlert = () => setShowDeleteAlert(!showDeleteAlert);
+
+    const [editMode, setEditMode] = useState(false)
+    const toggleEditMode = () => setEditMode(!editMode)
+
+    const [categoryToEdit, setCategoryToEdit] = useState({})
+    const onNameChanged = e => setCategoryToEdit({id: categoryToEdit.id, name: e.target.value})
+
 
     const dispatch = useDispatch()
     const categories = useSelector(state => state.categories)
@@ -25,11 +32,33 @@ const CategoryList = () => {
         )
     }
 
+    const handleCategoryEditClick = (category) => {
+        setCategoryToEdit(category)
+        toggleEditMode()
+    }
+
+    const handleCategoryEditSaveClick = (category) => {
+        setCategoryToEdit({})
+        toggleEditMode()
+        dispatch(categoriesUpdated(categoryToEdit))
+    }
+
     const renderedCategories = categories.map(category => (
         <ListGroup.Item key={ category.id }>
             <Row>
-                <Col sm={10}>
-                    { category.name }
+                <Col sm={9}>
+                    { editMode && categoryToEdit.id === category.id ? 
+                    <Form.Control required type="text" value={ categoryToEdit.name } onChange={ onNameChanged } placeholder="Enter name for category" /> :
+                    category.name 
+                    }
+                </Col>
+
+                <Col sm={1}>
+                    {
+                        editMode && categoryToEdit.id === category.id ? 
+                        <FaSave style={{color: "#333", cursor: "pointer"}} onClick={() => handleCategoryEditSaveClick(category)}/> :
+                        <FaPencilAlt style={{color: "#333", cursor: "pointer"}} onClick={() => handleCategoryEditClick(category)}/> 
+                    }
                 </Col>
 
                 <Col sm={1}>
--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -13,12 +13,17 @@ const categoriesSlice = createSlice({
         categoriesAdded(state, action) {
             state.push(action.payload)
         },
+        categoriesUpdated(state, action) {
+            const newState = state.filter(category => category.id !== action.payload.id)
+            newState.push(action.payload)
+            return newState
+        },
         categoriesRemoved(state, action) {
             return state.filter(category => category.id !== action.payload.id)
         }
     }
 })
 
-export const { categoriesAdded, categoriesRemoved } = categoriesSlice.actions
+export const { categoriesAdded, categoriesUpdated, categoriesRemoved } = categoriesSlice.actions
 
 export default categoriesSlice.reducer
